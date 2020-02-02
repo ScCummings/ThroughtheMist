76d312f66a9ba14e05b290b214087eba9349725e
nicholasmirchandani <nmirchandani@chapman.edu>
2020-02-02 07:25:18 -0800

    Fog can only be removed if the younger brother is near and now it disappears for both players over network

--- Assets/Scripts/ClearFog.cs
+++ Assets/Scripts/ClearFog.cs
@@ -6,12 +6,21 @@ using UnityEngine;
 public class ClearFog : MonoBehaviour
 {
     public bool FogHasBeenCleared = false;
-    private void OnTriggerStay2D(Collider2D collider)
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+    private void OnTriggerStay2D(Collider2D collider) {
+        if (collider.gameObject.tag == "YoungerBrother" && Input.GetKeyDown(KeyCode.Space)) {
             FogHasBeenCleared = true;
-            Destroy(gameObject);
+            if(GameManager.Instance != null && GameManager.Instance.isNetworked) {
+                GetComponent<PhotonView>().RequestOwnership();
+                PhotonView photonView = PhotonView.Get(GetComponent<PhotonView>());
+                photonView.RPC("DestroyFog", RpcTarget.All);
+            } else {
+                Destroy(gameObject);
+            }
+
         }
+    }
+
+    [PunRPC] public void DestroyFog() {
+        Destroy(gameObject);
     }
 }
