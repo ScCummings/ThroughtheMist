c53594ead6ddfea64f92fadc4708cf90e461be5c
nicholasmirchandani <nmirchandani@chapman.edu>
2020-02-01 23:12:44 -0800

    Blocks out Subway Level

---
+++ Assets/SubwayPuzzle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwayPuzzle : MonoBehaviour
+{
+    public bool isComplete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isComplete = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        isComplete = true;
+    }
+}
---
+++ Assets/SubwayPuzzleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubwayPuzzleManager : MonoBehaviour
+{
+
+    public SubwayPuzzle[] puzzles;
+
+    // Update is called once per frame
+    void Update() {
+        bool solved = true;
+        foreach (SubwayPuzzle sp in puzzles) {
+            if (!sp.isComplete) {
+                solved = false;
+            }
+        }
+        if(solved) {
+            SceneManager.LoadScene("Downtown");
+        }
+    }
+}
