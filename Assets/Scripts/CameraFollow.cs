ed0f3d3d48d25fd1930194f9975b88bf1df87f96
nicholasmirchandani <nmirchandani@chapman.edu>
2020-02-02 00:31:51 -0800

    Updates CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public float minDistance;
     public float cameraVelocity;
     public Vector3 offset; 
-    public GameObject target;
+    public GameObject[] targets;
     Vector3 targetPos;
 
     // Start is called before the first frame update
@@ -17,10 +17,16 @@ public class CameraFollow : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if (target) {
+        if (targets != null && targets.Length > 0) {
             Vector3 posNoZ = transform.position;
-            posNoZ.z = target.transform.position.z * -1;
-            Vector3 targetDirection = (target.transform.position - posNoZ);
+            posNoZ.z = targets[0].transform.position.z * -1;
+            Vector3 targetDirection;
+            if(targets.Length > 1) {
+                targetDirection = ((targets[0].transform.position + targets[1].transform.position) / 2 - posNoZ);
+            }
+            else {
+                targetDirection = targets[0].transform.position - posNoZ;
+            }
             cameraVelocity = targetDirection.magnitude * 5f;
             targetPos = transform.position + (targetDirection.normalized * cameraVelocity * Time.deltaTime);
             transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
