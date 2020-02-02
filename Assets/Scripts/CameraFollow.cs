3da1a183f946e0e5e2ba523ae9557e801347ce39
nicholasmirchandani <nmirchandani@chapman.edu>
2020-02-02 00:58:52 -0800

    Updates CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,6 +10,9 @@ public class CameraFollow : MonoBehaviour
     public GameObject[] targets;
     Vector3 targetPos;
 
+    public float maxSize = 10.0f;
+    public float minSize = 4.0f;
+
     // Start is called before the first frame update
     void Start() {
         targetPos = transform.position;
@@ -23,6 +26,14 @@ public class CameraFollow : MonoBehaviour
             Vector3 targetDirection;
             if(targets.Length > 1) {
                 targetDirection = ((targets[0].transform.position + targets[1].transform.position) / 2 - posNoZ);
+                Debug.Log("DISTANCE FROM A TO B: " + Vector2.Distance(targets[0].transform.position, targets[1].transform.position));
+                float targetSize = Mathf.Pow(Vector2.Distance(targets[0].transform.position, targets[1].transform.position), 0.7f);
+                if(targetSize < minSize) {
+                    targetSize = minSize;
+                } else if(targetSize > maxSize) {
+                    targetSize = maxSize;
+                }
+                GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, targetSize, 0.05f);
             }
             else {
                 targetDirection = targets[0].transform.position - posNoZ;
